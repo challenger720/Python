3b432a0f86abb5bdc69e2679d56064880be79d62
challenger720 <127072307+challenger720@users.noreply.github.com>
2025-11-01 09:17:34 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,19 @@ print(f"{name.lower()}")
 print(f"{name.title()}")
 print(f"{name.replace("a","@")}")
 
- File "c:\Users\Janice\CSOH\random.py", line 16, in <module>
-    DoOutput(name)
-    ~~~~~~~~^^^^^^
-  File "c:\Users\Janice\CSOH\random.py", line 10, in DoOutput
+import random
+
+def DoInput():
+    name = input("Input name: ")
+    return name
+
+def DoOutput(name):
+    output = f"Hello my dear {name}"
+    parts = list(output)
     randomized_parts = [ch.upper() if random.choice([True, False]) else ch.lower() for ch in parts]
-                                      ^^^^^^^^^^^^^
-AttributeError: module 'random' has no attribute 'choice' (consider renaming 'c:\Users\Janice\CSOH\random.py' since it has the same name as the standard library module named 'random' and prevents importing that standard library module)
+    randomized_output = "".join(randomized_parts)
+    print(randomized_output)
+
+if __name__ == '__main__':
+    name = DoInput()
+    DoOutput(name)
